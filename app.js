aa70633cb4f7416ba694d360ea661d6c421dcbe0
Dewanand khilar <dewanandkhilar@gmail.com>
2022-07-10 00:12:56 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 //jshint esversion:6
-const express = require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+const port = 3000;
 
 
 
-app.use(express.static('public'))   // eslint-disable-line no-unused-vars   no  strict-importants   no-unused-expressionSyntax  errors
-app.set('view engine', 'jade')   // eslint-disable-line no-unused-vars no-unused-expressionSyntax errors
-app.use(bodyParser.urlencoded({extended: true})) // eslint-disable-line no-unused-vars   no-unused-expressionSyntax Errors
+app.use(express.static('public'));   // eslint-disable-line no-unused-vars   no  strict-importants   no-unused-expressionSyntax  errors
+app.set('view engine', 'ejs');   // eslint-disable-line no-unused-vars no-unused-expressionSyntax errors
+app.use(bodyParser.urlencoded({extended: true})); // eslint-disable-line no-unused-vars   no-unused-expressionSyntax Errors
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/login', (req, res) => {    
+    res.render('login')
+});
+
+app.get('/register', (req, res) => {    
+        res.render('register')
+    });
+
+
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+      })
