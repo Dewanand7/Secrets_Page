1fad51b5f07fc27bf3478fbf38915f34b42d0aa5
Dewanand khilar <dewanandkhilar@gmail.com>
2022-07-10 00:27:44 +0530

    Updating moongose

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
 
 
-app.use(express.static('public'));   // eslint-disable-line no-unused-vars   no  strict-importants   no-unused-expressionSyntax  errors
-app.set('view engine', 'ejs');   // eslint-disable-line no-unused-vars no-unused-expressionSyntax errors
-app.use(bodyParser.urlencoded({extended: true})); // eslint-disable-line no-unused-vars   no-unused-expressionSyntax Errors
+app.use(express.static('public'));    // eslint-disable-line no-unused-vars   no  strict-importants   no-unused-expressionSyntax  errors
+app.set('view engine', 'ejs');        // eslint-disable-line no-unused-vars no-unused-expressionSyntax errors
+app.use(bodyParser.urlencoded({extended: true}));         // eslint-disable-line no-unused-vars   no-unused-expressionSyntax Errors
+
+mongoose.connect('mongodb://localhost://localhost:27017/userDB', {useNewUrlParser: true});              // eslint-disable-line no-unused-vars no-unused-expressionSyntax errorsMap
 
 
 app.get('/', (req, res) => {
@@ -28,4 +31,4 @@ app.get('/register', (req, res) => {
 
     app.listen(port, () => {
         console.log(`Example app listening at http://localhost:${port}`)
-      })
+      });
