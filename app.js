505c2cf00f02cbda48c46bef7cbfa8752f85269f
Dewanand khilar <dewanandkhilar@gmail.com>
2022-07-16 00:04:45 +0530

    UPdate

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -6,6 +7,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 const app = express();
 const port = 3000;
+
+console.log(process.env.API_KEY);
 
 
 
@@ -19,8 +22,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "ELSALVADORisaPassword."
-userSchema.plugin(encrypt,{ secret: secret , encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt,{ secret: secret , encryptedFields: ['password'] });
 
 const User = new mongoose.model('User',userSchema)
 //gggg
